b09a0eeb58a432c512ed2c1231bad41dee208934
Matus Murin <matusmurin@rarecrew.com>
2017-01-03 19:03:50 +0100

    add build_grid function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,27 @@ defmodule Identicon do
     input
     |> hash_string
     |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+  end
+
+  def build_grid(%Identicon.Image{bytes: bytes} = image) do
+    grid =
+      bytes
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({value, _index}) ->
+      rem(value, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def hash_string(input) do
